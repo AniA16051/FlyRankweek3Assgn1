7cc8636e1c50ed01979619ed6f2d7d3214f94f08
AniA16051 <anirudh16.ashok@gmail.com>
2026-08-03 08:47:45 +0530

    Stage2: Add tasks and 404 Handling

--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
-
+let tasks = [
+  { id: 1, title: "Do HackerRank", done: false },
+  { id: 2, title: "Finish DeathTroopers", done: true },
+  { id: 3, title: "Task3", done: false }
+];
 // Middleware to parse incoming JSON bodies
-app.use(express.json());
 
+app.use(express.json());
+//stage2
+app.get('/tasks', (req, res) => { res.json(tasks);});
+//stage2tasklist
+app.get('/tasks/:id', (req, res) => {
+  const taskId = parseInt(req.params.id);
+  const task = tasks.find(t => t.id === taskId);
+
+  if (!task) {
+    return res.status(404).json({ error: `Task ${taskId} not found` });
+  }
+
+  res.json(task);
+});
 // Stage 0 & 1: Root endpoint describing your API
 app.get('/', (req, res) => {
   res.json({
